abaf81c3977bdb8a6631c917f2edbeabd18e09bc
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-07-26 11:04:30 -0700

    add encryption to user email password

--- models/User.js
+++ models/User.js
@@ -1,16 +1,40 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
+const validator = require('validator')
 
+const validateEmail = function(email) {
+    return validator.isEmail(email);
+}
 const UserSchema = new Schema({
+
     email: {
         type: String,
         unique: true,
         required: true,
-        lowercase: false
+        lowercase: false,
+        validate: [
+            validateEmail,
+            'Please enter a valid email address'
+        ]
     },
     password: {
         type: String,
         required: true
+    }
+})
+
+UserSchema.pre('save', async function(next) {
+    const user = this;
+    try {
+        const salt = await bcrypt.genSalt()
+        console.log('salt', salt);
+        const hash = await bcrypt.hash(user.password, salt);
+        console.log('hash', hash)
+        user.password = hash;
+        next();
+    } catch (e) {
+        return next(e);
     }
 })
 
