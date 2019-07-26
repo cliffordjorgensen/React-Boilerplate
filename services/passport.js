ab927b021e48828e771b904cdf049852ae15e9ba
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-07-26 12:58:34 -0700

    configure jwt strategy

--- services/passport.js
+++ services/passport.js
@@ -32,13 +32,38 @@ const localLogin = new LocalStrategy(localOptions, async(email, password, done) 
 // We need to tell our strategy where to look for the token
 
 const jwtOptions = {
-    // tells jwt strategy that whenever a req cpomes in
-    // and we want to pass[port to handl it
-    // It needs to look in the header, fot the property called 'authorization'
+    // tells jwt strategy that whenever a request comes in
+    // and we want to passport to handle it
+    // It needs to look in the header, for the property called 'authorization'
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     // tells jwt strategy what secret we used to encode the token 
     // so that it ccan decode it
     secretOrKey: config.secrect
 }
 
+// we are going to get the payload arguement from an incoming request
+// the payload argument is coming from the function that we willl create in authRoutes
+// done is the function we call once we tried to authenticate this user
+
+const jwtLogin = new JwtStrategy(jwtOptions, async(payload, done) => {
+    try {
+        const user = await User.findById(payload.sub);
+        if (user) {
+            done(null, user)
+        } else {
+            done(null, false)
+        }
+    } catch (e) {
+        done(e, false)
+    }
+})
+
+
+// This tells passport that we declared these strategies
+// The local login says we have strategy called 'local'
+// The jwt login says we have a strategy called jwt
+
+// When we say passport.authenticate('jwt')
+// passport will look for a strategy called 'jwt'
 passport.use(localLogin)
+passport.use(jwtLogin)
